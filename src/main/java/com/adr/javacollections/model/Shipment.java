b766ad7aa9ad2ad73dc2fc16f343ded63a454f32
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-05-13 10:30:10 -0500

    Shipment unit tests added

--- src/main/java/com/adr/javacollections/model/Shipment.java
+++ src/main/java/com/adr/javacollections/model/Shipment.java
@@ -7,9 +7,38 @@ import java.util.List;
 public class Shipment implements Iterable<Product> {
 
     private List<Product> productList = new ArrayList<>();
+    private List<Product> lightProductsList = new ArrayList<>();
+    private List<Product> heavyProductsList = new ArrayList<>();
 
     public void add(Product product) {
         productList.add(product);
+    }
+
+    public void replace(Product oldProduct, Product newProduct) {
+
+        int productIndex = productList.indexOf(oldProduct);
+
+        if (productIndex != -1) {
+            productList.set(productIndex, newProduct);
+        }
+    }
+
+    public void prepare() {
+        productList.forEach(product -> {
+            if (product.getWeight() < 20) {
+                lightProductsList.add(product);
+            } else {
+                heavyProductsList.add(product);
+            }
+        });
+    }
+
+    public List<Product> getLightVanProducts() {
+        return lightProductsList;
+    }
+
+    public List<Product> getHeavyVanProducts() {
+        return heavyProductsList;
     }
 
     @Override
--- src/test/java/com/adr/javacollections/model/ShipmentTest.java
+++ src/test/java/com/adr/javacollections/model/ShipmentTest.java
@@ -17,10 +17,9 @@ public class ShipmentTest {
 	public static void initObjects() {
 
 		shipment = new Shipment();
+		window = new Product("Window", 10);
 		door = new Product("Wooden door", 35);
 		floorPanel = new Product("Floor panel", 25);
-		window = new Product("Window", 10);
-
 	}
 
 	@Test
@@ -30,7 +29,39 @@ public class ShipmentTest {
 		shipment.add(window);
 		
 		assertThat(shipment, hasItems(door, window));
-		
 	}
 
+	@Test
+	public void shouldReplaceitems(){
+
+		shipment.add(door);
+		shipment.add(window);
+
+		shipment.replace(door, floorPanel);
+
+		assertThat(shipment, hasItems(window, floorPanel));
+	}
+
+	@Test
+	public void shouldNotReplaceMissingItems(){
+
+		shipment.add(window);
+
+		shipment.replace(door, floorPanel);
+
+		assertThat(shipment, hasItem(window));
+	}
+
+	@Test
+	public void shouldIdentifyVanRequirements(){
+
+		shipment.add(window);
+		shipment.add(floorPanel);
+		shipment.add(door);
+
+		shipment.prepare();
+
+		assertThat(shipment.getLightVanProducts(), hasItem(window));
+		assertThat(shipment.getHeavyVanProducts(), hasItems(door, floorPanel));
+	}
 }
